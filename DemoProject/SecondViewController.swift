f1b4afc9210588a9849968e161475e762d235e40
SaiKiranPanuganti <saikiranpanuganti92@gmail.com>
2021-08-09 20:47:04 +0530

    Second View Controller added

---
+++ DemoProject/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  DemoProject
+//
+//  Created by SaiKiran Panuganti on 09/08/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+ 
+
+}
